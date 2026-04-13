20fc4b39ad47782446a84d2c3234d7c375fc6ccb
Giulio Pedicone <pediconegiulio02@gmail.com>
2026-04-13 09:10:56 +0200

    Init: creata struttura base frontend, backend e dataset

---
+++ backend/omniprice/src/main/java/com/unict/dmi/omniprice/OmnipriceApplication.java
@@ -0,0 +1,13 @@
+package com.unict.dmi.omniprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnipriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnipriceApplication.class, args);
+	}
+
+}
---
+++ backend/omniprice/src/test/java/com/unict/dmi/omniprice/OmnipriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unict.dmi.omniprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnipriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
